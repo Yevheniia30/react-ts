62e798d8a73fca10c3c77ad92a26dca908e3139b
dn301086siv <yevheniia.suska@privatbank.ua>
2024-01-22 00:20:53 +0200

    #master - init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form/Form';
+
+function getFirstElement<ElementType>(array: ElementType[]) {
+  return array[0];
+}
+
+const numbers = [1, 2, 3];
+const firstNum = getFirstElement(numbers);
+
+const strings = ['qwe', 'asd', 'zxc'];
+const firstStr = getFirstElement(strings);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form />
     </div>
   );
 }
