c09fe594ce5df852d7c4197052110b4899a91c48
dn301086siv <yevheniia.suska@privatbank.ua>
2024-01-23 23:36:12 +0200

    #master - added storybook

---
+++ src/stories/App.stories.ts
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from './Button';
+import App from '../App';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Example/App',
+  component: App,
+} satisfies Meta<typeof App>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
+
+// export const Secondary: Story = {
+//   args: {
+//     label: 'Button',
+//   },
+// };
+//
+// export const Large: Story = {
+//   args: {
+//     size: 'large',
+//     label: 'Button',
+//   },
+// };
+//
+// export const Small: Story = {
+//   args: {
+//     size: 'small',
+//     label: 'Button',
+//   },
+// };
